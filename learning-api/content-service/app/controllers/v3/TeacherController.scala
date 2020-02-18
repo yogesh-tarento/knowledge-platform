f66d6525a2e41a2b0b862bdbdb1c277875946cfa
Pradyumna Nagendra <pradyumnan@ilimi.in>
2020-02-18 15:23:33 +0530

    Issue #devcon feat: School API

--- learning-api/content-service/app/controllers/v3/TeacherController.scala
+++ learning-api/content-service/app/controllers/v3/TeacherController.scala
@@ -6,10 +6,11 @@ import akka.actor.{ActorRef, ActorSystem}
 import controllers.BaseController
 import javax.inject.{Inject, Named}
 import play.api.mvc.ControllerComponents
-import utils.{ActorNames, ApiId}
+import utils.ActorNames
 
 import scala.collection.JavaConversions._
 import scala.concurrent.ExecutionContext
+import scala.sys.process._
 
 class TeacherController @Inject()(@Named(ActorNames.SCHOOL_ACTOR) schoolActor: ActorRef, cc: ControllerComponents, actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends BaseController(cc) {
 
@@ -58,5 +59,11 @@ class TeacherController @Inject()(@Named(ActorNames.SCHOOL_ACTOR) schoolActor: A
         val contentRequest = getRequest(content, headers, "getUserData")
         setRequestContext(contentRequest, version, objectType, schemaName)
         getResult("api.teacher.profile", schoolActor, contentRequest)
+    }
+
+    def notifyBatch()  = Action {
+        val cmd = Seq("curl", "-X", "POST", s"https://onesignal.com/api/v1/notifications", "-H", "Content-Type: application/json", "-H", "authorization: Basic MDY4YjY1YTctNWRjMC00MjkxLWFiMTQtYzczMDg5MzI2Yzhm", "-d", "{\"app_id\":\"6e98f8cf-67fe-4798-93b9-97955e4858fc\",\"filters\":[{\"field\":\"tag\",\"key\":\"teachers\",\"relation\":\"=\",\"value\":\"true\"}],\"headings\":{\"en\":\"My Teacher Image notification title\"},\"contents\":{\"en\":\"My Teacher notification content\"},\"data\":{\"task\":\"Sent through api Teacher\"},\"big_picture\":\"https://img.onesignal.com/n/37326fcc-2baa-45da-891c-ca9454a64957.png\",\"large_icon\":\"https://img.onesignal.com/n/b5cadcf0-1297-4489-b865-545b421d8c5d.png\"}")
+        val result = cmd.!!
+        Ok(result).as("application/json")
     }
 }
